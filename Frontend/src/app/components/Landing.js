c4a7c90d18c73c39bc7d11b3e1ea9e91d3eabea9
Daan Zwaenepoel <daan.zwaenepoel@student.odisee.be>
2017-04-10 20:36:09 +0200

    In a world far far away...

--- Frontend/src/app/components/Landing.js
+++ Frontend/src/app/components/Landing.js
@@ -10,15 +10,18 @@ export class Landing extends React.Component {
     render() {
         return (
             <section className="bgImg">
-              
+            
                 <div className="cardLanding">
-                    <div>
+                    
                         <div className="card-container">
                              <div className="title-container">
                                  <h1>Easyshop</h1>
                              </div>
+                             <a className="link" href="#">&#60; Meld u aan als winkel</a>
+                            <div className="icon"></div>
+                             <a className="link" href="#">Start winkelen &#62;</a>
                         </div>
-                    </div>
+                    
                    
                 </div>
 
